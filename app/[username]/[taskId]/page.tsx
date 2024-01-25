368a2f945bb54d23e98b8583b4b61fb6f3e01a6d
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-01-25 21:13:43 +0300

    feat: task done status shown in card

--- app/[username]/[taskId]/page.tsx
+++ app/[username]/[taskId]/page.tsx
@@ -20,7 +20,14 @@ import { db } from '~/lib/db';
 import { formatDate } from '~/lib/utils';
 import { PAGES } from '~/lib/constants';
 
-async function getTask(username: string, taskId: string) {
+export default async function TaskPage({
+  params
+}: {
+  params: {
+    username: string;
+    taskId: string;
+  };
+}) {
   const session = await getServerSession(authOptions);
 
   if (!session) {
@@ -30,38 +37,18 @@ async function getTask(username: string, taskId: string) {
   const task = await db.task.findUnique({
     where: {
       author: {
-        username
+        username: params.username
       },
-      id: taskId
+      id: params.taskId
     }
   });
-
-  // I'm appologizing
-  return {
-    task,
-    sessionUsername: session.user.username
-  };
-}
-
-export default async function TaskPage({
-  params
-}: {
-  params: {
-    username: string;
-    taskId: string;
-  };
-}) {
-  const { task, sessionUsername } = await getTask(
-    params.username,
-    params.taskId
-  );
 
   return (
     <Card className='w-full max-w-[625px]'>
       {task ? (
         <>
           <CardHeader>
-            <CardTitle>{task.title}</CardTitle>
+            <CardTitle>{`${task.title}`}</CardTitle>
           </CardHeader>
           <CardContent>
             <div className='grid w-full items-center gap-4'>
@@ -70,18 +57,21 @@ export default async function TaskPage({
                 readOnly
                 value={task.description || 'no description'}
               />
-              {params.username !== sessionUsername && (
+              {params.username !== session.user.username && (
                 <Badge className='w-max'>{`${params.username} shared the task with you 🤝`}</Badge>
               )}
-              {!!task.due ? (
-                <time className='flex items-center gap-2 text-sm'>
-                  <Icons.calendarClock className='size-4' />
-                  <span className='sm:hidden'>{`${formatDate(task.due.toString(), 'short')}`}</span>
-                  <span className='hidden sm:block'>{`${formatDate(task.due.toString())}`}</span>
-                </time>
-              ) : (
-                <span className='text-sm'>no due date</span>
-              )}
+              <div className='flex items-center justify-between text-sm'>
+                {!!task.due ? (
+                  <time className='flex items-center gap-2 '>
+                    <Icons.calendarClock className='size-4' />
+                    <span className='sm:hidden'>{`${formatDate(task.due.toString(), 'short')}`}</span>
+                    <span className='hidden sm:block'>{`${formatDate(task.due.toString())}`}</span>
+                  </time>
+                ) : (
+                  <span>no due date</span>
+                )}
+                <span>{`Done status: ${task.done ? 'done ✅' : 'in progress ⏳'}`}</span>
+              </div>
             </div>
           </CardContent>
         </>
@@ -92,7 +82,7 @@ export default async function TaskPage({
       )}
       <CardFooter>
         <Button variant='outline' asChild>
-          <Link href={`/${sessionUsername}`}>Go back</Link>
+          <Link href={`/${session.user.username}`}>Go back</Link>
         </Button>
       </CardFooter>
     </Card>

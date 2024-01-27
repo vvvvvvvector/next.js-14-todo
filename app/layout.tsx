6a2b1cc608485ea21834d8619ac4a4c201f0028a
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-01-27 12:23:35 +0300

    feat: viewport options, no scale on inputs on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { type Metadata } from 'next';
+import type { Viewport, Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from '~/components/ui/sonner';
 
@@ -10,6 +10,12 @@ const inter = Inter({ subsets: ['latin'] });
 
 type RootLayoutProps = {
   children: React.ReactNode;
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  userScalable: false
 };
 
 export const metadata: Metadata = {
